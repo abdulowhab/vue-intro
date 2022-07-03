89ee74a691d44e1d84f8ba0aa5c376f0566a909a
Md. Abdul Owhab <owhab@pigeon-soft.com>
2022-07-04 00:04:00 +0600

    working with for loop, list and array rendering

--- main.js
+++ main.js
@@ -8,6 +8,18 @@ const app = Vue.createApp({
             url: "https://abdulowhab.netlify.app",
             class: "isActive",
             inventory: 100,
+            details: ["50% cotton", "30% wool", "20% polyester"],
+
+            variants: [
+                {
+                    id: 01, color: "green"
+                },
+                {
+                    id: 02, color: "blue"
+                }
+            ],
+
+            sizes: ["L", "XL", "XXL", "M"],
 
 
 
