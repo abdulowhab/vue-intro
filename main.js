b2980cb49ec907ed3243e5fe2150caac0197165e
Md. Abdul Owhab <owhab@pigeon-soft.com>
2022-07-03 22:53:00 +0600

    completed vue m lesson 2

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const app = Vue.createApp({
     data() {
         return {
             product: "Socks",
+            desc: "Etymology. The modern English word sock is derived from the Old English word socc, meaning light slipper. This comes from the Latin soccus, a term to describe a light, low-heeled shoe worn by Roman comic actors, and deriving from the Ancient Greek word sykchos"
             
         }
     },
