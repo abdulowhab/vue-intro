11e96aedecdf4057370c38feb1a30c46fb1e4fcb
Md. Abdul Owhab <owhab@pigeon-soft.com>
2022-07-03 23:31:09 +0600

    attribute binding in vue

--- main.js
+++ main.js
@@ -2,7 +2,13 @@ const app = Vue.createApp({
     data() {
         return {
             product: "Socks",
-            desc: "Etymology. The modern English word sock is derived from the Old English word socc, meaning light slipper. This comes from the Latin soccus, a term to describe a light, low-heeled shoe worn by Roman comic actors, and deriving from the Ancient Greek word sykchos"
+            desc: "Etymology. The modern English word sock is derived from the Old English word socc, meaning light slipper. This comes from the Latin soccus, a term to describe a light, low-heeled shoe worn by Roman comic actors, and deriving from the Ancient Greek word sykchos",
+            image: "./assets/images/socks_green.jpg",
+            alt: "Green Socks Image",
+            url: "https://abdulowhab.netlify.app",
+            class: "isActive"
+
+
             
         }
     },
